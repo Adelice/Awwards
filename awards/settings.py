a90e2f8f7cab218a6c8f86c98cc046e4784ae018
Adelice <nancytoniw@gmail.com>
2019-04-01 17:01:01 +0200

    install restframework

--- awards/settings.py
+++ awards/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'bootstrap3',
     'aww',
     'bootstrap4',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
--- aww/admin.py
+++ aww/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Project,Image,Location,Profile
+from .models import Project,Location,Profile
 admin.site.register(Project)
 admin.site.register(Profile)
-admin.site.register(Image)
+
 admin.site.register(Location)
