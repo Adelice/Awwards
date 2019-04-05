698cacaf26e5a3819e272a73d291eaa150dd7155
Adelice <nancytoniw@gmail.com>
2019-04-05 17:01:23 +0200

    save changes

--- aww/views.py
+++ aww/views.py
@@ -14,7 +14,7 @@ from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework.response import Response
 from .permissions import IsAdminOrReadOnly
-from .serializers import ProfileSerializer
+from .serializers import ProfileSerializer,ProjectSerializer
 
 # Create your views here.
 
